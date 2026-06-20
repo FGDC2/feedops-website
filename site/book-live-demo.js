38ddb524811231a77f1bb6770ef202b0a977ab55
FGDC2 <50128846+FGDC2@users.noreply.github.com>
2026-06-20 18:28:01 +0930

    Defer booking calendar for speed

--- site/book-live-demo.js
+++ site/book-live-demo.js
@@ -36,43 +36,64 @@
       meeting_source: "hubspot",
       email: contact.email
     });
+
+    loadReferralScript();
   });
 
   document.querySelectorAll(".meetings-iframe-container").forEach(function (container) {
+    var loadButton = container.querySelector("[data-load-calendar]");
+
     function markLoaded() {
       container.classList.add("is-loaded");
       container.setAttribute("aria-busy", "false");
     }
 
-    function watchForIframe() {
-      var iframe = container.querySelector("iframe");
-      if (!iframe) {
+    function loadCalendar() {
+      if (container.querySelector("iframe")) {
         return;
       }
 
+      container.classList.add("is-loading");
+      container.setAttribute("aria-busy", "true");
+
+      var iframe = document.createElement("iframe");
+      iframe.src = container.getAttribute("data-src");
+      iframe.title = "Book a FeedOps demo calendar";
+      iframe.width = "100%";
+      iframe.loading = "lazy";
+      iframe.setAttribute("data-hs-ignore", "true");
+      iframe.style.minWidth = "312px";
+      iframe.style.minHeight = "615px";
+      iframe.style.borderWidth = "0";
       iframe.addEventListener("load", markLoaded, { once: true });
       window.setTimeout(markLoaded, 900);
+      container.appendChild(iframe);
     }
 
-    watchForIframe();
-
-    var observer = new MutationObserver(watchForIframe);
-    observer.observe(container, { childList: true, subtree: true });
+    if (loadButton) {
+      loadButton.addEventListener("click", loadCalendar);
+    }
   });
 
-  window.referralJS = window.referralJS || {};
-  window.referralJS.scriptConfig = {
-    parameters: {
-      src: "//feedops.referralrock.com/ReferralSdk/referral.js",
-      transactionKey: "7301c474-c959-4e64-9114-a9bde03e3465"
+  function loadReferralScript() {
+    if (document.getElementById("RR_DIVID_V5")) {
+      return;
     }
-  };
 
-  var referralScript = document.createElement("script");
-  var firstScript = document.getElementsByTagName("script")[0];
-  referralScript.async = true;
-  referralScript.src = window.referralJS.scriptConfig.parameters.src + "?referrer=" + encodeURIComponent(window.location.origin + window.location.pathname).replace(/[!'()*]/g, escape);
-  referralScript.id = "RR_DIVID_V5";
-  referralScript.setAttribute("transactionKey", window.referralJS.scriptConfig.parameters.transactionKey);
-  firstScript.parentNode.insertBefore(referralScript, firstScript);
+    window.referralJS = window.referralJS || {};
+    window.referralJS.scriptConfig = {
+      parameters: {
+        src: "//feedops.referralrock.com/ReferralSdk/referral.js",
+        transactionKey: "7301c474-c959-4e64-9114-a9bde03e3465"
+      }
+    };
+
+    var referralScript = document.createElement("script");
+    var firstScript = document.getElementsByTagName("script")[0];
+    referralScript.async = true;
+    referralScript.src = window.referralJS.scriptConfig.parameters.src + "?referrer=" + encodeURIComponent(window.location.origin + window.location.pathname).replace(/[!'()*]/g, escape);
+    referralScript.id = "RR_DIVID_V5";
+    referralScript.setAttribute("transactionKey", window.referralJS.scriptConfig.parameters.transactionKey);
+    firstScript.parentNode.insertBefore(referralScript, firstScript);
+  }
 })();
